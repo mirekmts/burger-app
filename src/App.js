6e94b5c2483f30532e49bbb508472d75f35e0abd
mirekmts <mirekmts@gmail.com>
2018-01-22 18:14:17 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        czesc
+      </div>
+    );
+  }
+}
+
+export default App;
