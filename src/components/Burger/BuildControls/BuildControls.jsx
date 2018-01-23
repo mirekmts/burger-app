05f90f14fcd39d52e266a8b6e96bb62e29f08e15
mirekmts <mirekmts@gmail.com>
2018-01-23 17:33:27 +0100

    add a action to button less and more, add a order button

--- src/components/Burger/BuildControls/BuildControls.jsx
+++ src/components/Burger/BuildControls/BuildControls.jsx
@@ -8,13 +8,22 @@ const controls = [
     { label: 'Bacon', type: 'bacon' },
     { label: 'Cheese', type: 'cheese' },
     { label: 'Meat', type: 'meat' },
-]
+];
 
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
+        <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
         {controls.map(ctrl => (
-            <BuildControl key={ctrl.label} label={ctrl.label} />
+            <BuildControl 
+                key={ctrl.label} 
+                label={ctrl.label}
+                disabled={props.disabled[ctrl.type]}
+                added={() => props.ingredientAdded(ctrl.type)}
+                remove={() => props.ingredientRemoved(ctrl.type)} />
         ))}
+        <button 
+            className={classes.OrderButton}
+            disabled={!props.purchasable}>ORDER NOW</button>
     </div>
 );
 
--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -6,14 +6,13 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 const burger = (props) => {
     let transformIngredients = Object.keys(props.ingredients)
         .map(igKey => {
-            return [...Array(props.ingredients[igKey])].map((_, i) => {
+            return [...Array( props.ingredients[igKey] )].map((_, i) => {
                 return <BurgerIngredient key={igKey + i} type={igKey} />
             });
         })
         .reduce((arr, el) => {
             return arr.concat(el)
         }, []);
-        
     if (transformIngredients.length === 0) {
         transformIngredients = <p>Please start adding ingredients</p>
     }
