288ec756333983a22cf77d47a0539470ee41bedd
mirekmts <mirekmts@gmail.com>
2018-01-23 17:52:41 +0100

    add a modal for order

---
+++ src/components/Burger/OrderSummary/OrderSummary.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import AuxW from '../../../hoc/AuxW';
+
+const orderSummary = (props) => {
+    const ingridientsSummary = Object.keys(props.ingredients)
+        .map( igKey => {
+            return (
+                <li keys={igKey}>
+                    <span style={{textTransform: 'capitalize'}}>{igKey}</span> : {props.ingredients[igKey]}
+                </li> );
+        })
+
+    return (
+        <AuxW>
+            <h3>Your Order</h3>
+            <p>A delcious burger with following ingredients:</p>
+            <ul>
+                {ingridientsSummary}
+            </ul>
+            <p>Continue to Checkout?</p>
+        </AuxW>
+    )
+
+};
+
+export default orderSummary;
