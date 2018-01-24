a008c7a925bbaf85013feacf52b55055c78635ef
mirekmts <mirekmts@gmail.com>
2018-01-24 18:13:04 +0100

    add a expandle menu with backdrop

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../hoc/AuxW';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
+    state = {
+        showSiderDrawer: true
+    }
 
-export default layout;
+    sideDrawerClosedHandler = () => {
+        this.setState({showSiderDrawer: false});
+    };
+
+    sideDrawerTogglehandler = () => {
+        this.setState((prevState) => {
+            return { showSiderDrawer: !prevState.showSiderDrawer };
+        });
+    }
+
+    render () {
+        return (
+            <Aux>
+                <Toolbar drawerToggleClicked={this.sideDrawerTogglehandler} />
+                <SideDrawer open={this.state.showSiderDrawer} closed={this.sideDrawerClosedHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+}
+
+export default Layout;
